0f82716909b1c0d3c34e66e4089aef8886bd79db
icchii <i2s7.chacha@gmail.com>
2019-12-16 04:41:04 +0900

    webView表示用のViewControllerへ画面遷移を接続

---
+++ SearchGithubUserApp/View/DetailWebViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailWebViewController.swift
+//  SearchGithubUserApp
+//
+//  Created by 市川星磨 on 2019/12/16.
+//  Copyright © 2019 市川 星磨. All rights reserved.
+//
+
+import UIKit
+
+class DetailWebViewController: UIViewController {
+
+    public var url: URL?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print(url)
+    }
+}
